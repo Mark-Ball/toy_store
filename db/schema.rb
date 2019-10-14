01952c0ee6788396fb0310695722cf76b947a810
Mark-Ball <markball111@gmail.com>
2019-10-14 16:14:09 +1100

    set up table in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_14_050600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "toys", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "date"
+    t.string "user"
+  end
 
 end
