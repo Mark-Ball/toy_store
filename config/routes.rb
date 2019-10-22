4fd2fd13bb58a554388dfe6566bd5a0ee13a1efc
Mark-Ball <markball111@gmail.com>
2019-10-22 14:00:49 +1100

    installed devise gem and added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "pages#home", as: "root"
   get "/toys", to: "toys#index", as: "toys"
   post "/toys", to: "toys#create"
   get "/toys/new", to: "toys#new", as: "new_toy"
